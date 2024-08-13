bd4b9d4210c149c5b1b2e0db145989517a7a5a87
Bruce Zhu <bruce@itrazotracetech.com>
2024-08-13 23:24:04 +1000

    remove unused code

--- main.py
+++ main.py
@@ -44,10 +44,6 @@ def get_court_url(court_name):
 def main():
   options = Options()
   options.add_argument("--headless")  
-  options.add_argument("--no-sandbox") # Bypass OS security model
-  options.add_argument("--disable-dev-shm-usage") # Overcome limited resource problems
-  options.add_argument('--disable-gpu')
-  options.add_argument('--remote-debugging-port=9222')
 
   # Setup the WebDriver
   driver = webdriver.Chrome(options=options)
