cfd5b0a0b413fb285e857ad1edc1b990238f2446
Bruce Zhu <brucezzhu@gmail.com>
2024-08-22 08:23:33 +1000

    fix syntax

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def main():
   try:
     for court_name in FACILITY_ID:
       availability_data = check_availability(driver, get_court_url(court_name), court_name)
-      if len(availability_data.data) > 0:
+      if len(availability_data['data']) > 0:
         data.append(availability_data)
   except Exception as e:
     logging.error(f"An error occurred: {e}")
